6800b1d5bf7b3214b65a6142d719c32f7291d8ff
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-12-13 15:45:21 -0800

    Better abstractions.

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,14 @@
-use egui::{Pos2, Rect, Vec2, Mesh, Shape, Color32, Stroke, TextStyle, ScrollArea, Sense, NumExt, Align2};
+use egui::{
+    Align2, Color32, Mesh, NumExt, Pos2, Rect, ScrollArea, Sense, Shape, Stroke, TextStyle, Vec2,
+};
 use rand::Rng;
-use std::time::{Duration, Instant};
 use serde::{Deserialize, Serialize};
+use std::time::{Duration, Instant};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Deserialize, Serialize)]
+pub struct Timestamp(pub u64 /* ns */);
+
+#[derive(Default, Deserialize, Serialize)]
 pub struct Slot {
     expanded: bool,
     short_name: String,
@@ -11,10 +16,96 @@ pub struct Slot {
     max_rows: u64,
 }
 
+const UNEXPANDED_ROWS: u64 = 4;
+
+impl Slot {
+    fn ui(&mut self, ui: &mut egui::Ui, rect: Rect, row_height: f32) {
+        let response = ui.allocate_rect(rect, egui::Sense::click());
+
+        let font_id = TextStyle::Body.resolve(ui.style());
+        let visuals = ui.style().interact_selectable(&response, false);
+        ui.painter()
+            .rect(rect, 0.0, visuals.bg_fill, visuals.bg_stroke);
+        ui.painter().text(
+            rect.min + Vec2::new(4.0, 4.0),
+            Align2::LEFT_TOP,
+            &self.short_name,
+            font_id.clone(),
+            visuals.text_color(),
+        );
+    }
+
+    fn rows(&self) -> u64 {
+        if self.expanded {
+            self.max_rows
+        } else {
+            UNEXPANDED_ROWS
+        }
+    }
+}
+
+#[derive(Default, Deserialize, Serialize)]
+pub struct Window {
+    slots: Vec<Slot>,
+    min_time: Timestamp,
+    max_time: Timestamp,
+    window_start: Timestamp,
+    window_stop: Timestamp,
+}
+
+impl Window {
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        // Use body font to figure out how tall to draw rectangles.
+        let font_id = TextStyle::Body.resolve(ui.style());
+        let row_height = ui.fonts().row_height(&font_id);
+
+        ScrollArea::vertical()
+            .auto_shrink([false; 2])
+            .show_viewport(ui, |ui, viewport| {
+                // First pass to figure out how many rows we have in total
+                let mut total_rows = 0;
+                for slot in &self.slots {
+                    total_rows += slot.rows();
+                }
+
+                ui.set_height(row_height * total_rows as f32);
+                ui.set_width(ui.available_width());
+
+                let first_row = (viewport.min.y / row_height).floor().at_least(0.0) as u64;
+                let last_row = (viewport.max.y / row_height).ceil() as u64 + 1;
+                let last_row = last_row.at_most(total_rows);
+
+                // Second pass to draw those that intersect with the window
+                let mut row = 0;
+                for slot in &mut self.slots {
+                    let start_row = row;
+                    row += slot.rows();
+
+                    // Cull out-of-view slots
+                    if row < first_row {
+                        continue;
+                    } else if start_row > last_row {
+                        break;
+                    }
+
+                    let y_min = ui.min_rect().top() + start_row as f32 * row_height;
+                    let y_max = ui.min_rect().top() + row as f32 * row_height;
+
+                    let rect = ui
+                        .min_rect()
+                        .intersect(Rect::everything_below(y_min))
+                        .intersect(Rect::everything_above(y_max));
+
+                    slot.ui(ui, rect, row_height);
+                }
+            });
+    }
+}
+
 #[derive(Deserialize, Serialize)]
 #[serde(default)] // deserialize missing fields as default value
 pub struct ProfViewer {
-    slots: Vec<Slot>,
+    window: Window,
 
     #[cfg(not(target_arch = "wasm32"))]
     #[serde(skip)]
@@ -24,7 +115,7 @@ pub struct ProfViewer {
 impl Default for ProfViewer {
     fn default() -> Self {
         Self {
-            slots: Vec::new(),
+            window: Window::default(),
             #[cfg(not(target_arch = "wasm32"))]
             last_update: Instant::now(),
         }
@@ -47,10 +138,10 @@ impl ProfViewer {
 
         let mut rng = rand::thread_rng();
         const N: i32 = 16;
-        result.slots.clear();
+        result.window.slots.clear();
         for i in 0..N {
             let rows: u64 = rng.gen_range(0..64);
-            result.slots.push(Slot {
+            result.window.slots.push(Slot {
                 expanded: false,
                 short_name: format!("s{}", i),
                 long_name: format!("slot {}", i),
@@ -75,7 +166,7 @@ impl eframe::App for ProfViewer {
     /// Called each time the UI needs repainting.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self {
-            slots,
+            window,
             #[cfg(not(target_arch = "wasm32"))]
             last_update,
         } = self;
@@ -124,116 +215,8 @@ impl eframe::App for ProfViewer {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Test Heading");
-            view_area(ui, slots);
+            window.ui(ui);
             egui::warn_if_debug_build(ui);
         });
     }
 }
-
-pub fn view_area(ui: &mut egui::Ui, slots: &Vec<Slot>) {
-    // Use body font to figure out how tall to draw rectangles.
-    let font_id = TextStyle::Body.resolve(ui.style());
-    let row_height = ui.fonts().row_height(&font_id);
-
-    const UNEXPANDED_ROWS: u64 = 4;
-
-    ScrollArea::vertical()
-        .auto_shrink([false; 2])
-        .show_viewport(ui, |ui, viewport| {
-            // First pass to figure out how many rows we have in total
-            let mut total_rows = 0;
-            for slot in slots {
-                if slot.expanded {
-                    total_rows += slot.max_rows;
-                } else {
-                    total_rows += UNEXPANDED_ROWS;
-                }
-            }
-
-            ui.set_height(row_height * total_rows as f32);
-            ui.set_width(ui.available_width());
-
-            let first_row = (viewport.min.y / row_height).floor().at_least(0.0) as u64;
-            let last_row = (viewport.max.y / row_height).ceil() as u64 + 1;
-            let last_row = last_row.at_most(total_rows);
-
-            // let visuals = ui.style().interact_selectable(&response, false);
-            let visuals = ui.style().noninteractive(); // Hack, need to figure out how to make this responsive
-            let font_id = TextStyle::Body.resolve(ui.style());
-
-            // Second pass to draw those that intersect with the window
-            let mut row = 0;
-            let mut used_rect = Rect::NOTHING;
-            for slot in slots {
-                let start_row = row;
-                if slot.expanded {
-                    row += slot.max_rows;
-                } else {
-                    row += UNEXPANDED_ROWS;
-                }
-
-                // Prune slots out of window
-                if start_row > last_row || row < first_row {
-                    continue;
-                }
-
-                let y_min = ui.min_rect().top() + start_row as f32 * row_height;
-                let y_max = ui.min_rect().top() + row as f32 * row_height;
-
-                let slot_rect = ui.min_rect().intersect(Rect::everything_below(y_min)).intersect(Rect::everything_above(y_max));
-
-                ui.painter().rect(
-                    slot_rect,
-                    0.0,
-                    Color32::GREEN/*visuals.bg_fill*/, visuals.bg_stroke,
-                    // Color32::GREEN, Color32::BLACK
-                    //visuals.bg_fill, visuals.bg_stroke
-                );
-                ui.painter().text(
-                    slot_rect.min,
-                    Align2::LEFT_TOP,
-                    slot.short_name.clone(),
-                    font_id.clone(),
-                    visuals.text_color(),
-                );
-                used_rect = used_rect.union(slot_rect);
-            }
-
-            ui.allocate_rect(used_rect, Sense::hover());
-        });
-}
-
-// pub fn slot_ui(ui: &mut egui::Ui, slots: &Slot) -> egui::Response {
-//     let (rect, mut response) = ui.allocate_exact_size(ui.available_size(), egui::Sense::click());
-//     let hover = response.hover_pos(); // where is the mouse hovering (if any)
-//     if response.clicked() {
-//         // This is the click handler
-//     }
-//     if ui.is_rect_visible(rect) {
-//         // Draw the widget
-//         let visuals = ui.style().interact_selectable(&response, false);
-//         // let rect = rect.expand(visuals.expansion);
-//         for r in rects {
-//             let r2 = Rect::from_min_max(
-//                 rect.lerp(r.r.left_top().to_vec2()),
-//                 rect.lerp(r.r.right_bottom().to_vec2()),
-//             );
-//             if let Some(h) = hover {
-//                 if r2.contains(h) {
-//                     let r2 = r2.expand(visuals.expansion);
-//                     ui.painter()
-//                         .rect(r2, 0.0, r.c, /*visuals.bg_fill,*/ visuals.bg_stroke);
-//                     continue;
-//                 }
-//             };
-//             // let r2 = r2.expand(visuals.expansion);
-//             ui.painter()
-//                 .rect(r2, 0.0, r.c, Stroke::NONE);
-//         }
-//     }
-//     response
-// }
-
-// pub fn slot(rects: &Slot) -> impl egui::Widget + '_ {
-//     move |ui: &mut egui::Ui| slot_ui(ui, rects)
-// }
