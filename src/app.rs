a56d11849b053c8c446f49358a17474667040b71
Elliott Slaughter <slaughter@cs.stanford.edu>
2022-12-17 10:32:19 -0800

    Multiple profile support.

--- src/app.rs
+++ src/app.rs
@@ -67,19 +67,10 @@ struct Panel<S: Entry> {
 }
 
 #[derive(Default, Deserialize, Serialize)]
-struct Settings {
-    row_height: f32,
-
-    // Visible time range
-    window_start: Timestamp,
-    window_stop: Timestamp,
-
+struct Config {
     // Node selection controls
     min_node: u64,
     max_node: u64,
-
-    #[serde(skip)]
-    rng: rand::rngs::ThreadRng,
 }
 
 #[derive(Default, Deserialize, Serialize)]
@@ -88,14 +79,30 @@ struct Window {
     panel: Panel<Panel<Panel<Slot>>>, // nodes -> kind -> proc/chan/mem
     min_time: Timestamp,
     max_time: Timestamp,
+    index: u64,
     kinds: Vec<String>,
-    settings: Settings,
+    config: Config,
+}
+
+#[derive(Default, Deserialize, Serialize)]
+struct Context {
+    row_height: f32,
+
+    // Visible time range
+    window_start: Timestamp,
+    window_stop: Timestamp,
+
+    #[serde(skip)]
+    rng: rand::rngs::ThreadRng,
 }
 
 #[derive(Default, Deserialize, Serialize)]
 #[serde(default)] // deserialize missing fields as default value
 pub struct ProfApp {
-    window: Window,
+    #[serde(skip)]
+    windows: Vec<Window>,
+
+    cx: Context,
 
     #[cfg(not(target_arch = "wasm32"))]
     #[serde(skip)]
@@ -142,9 +149,16 @@ trait Entry {
         }
     }
 
-    fn content(&mut self, ui: &mut egui::Ui, rect: Rect, viewport: Rect, settings: &mut Settings);
+    fn content(
+        &mut self,
+        ui: &mut egui::Ui,
+        rect: Rect,
+        viewport: Rect,
+        config: &mut Config,
+        cx: &mut Context,
+    );
 
-    fn height(&self, settings: &Settings) -> f32;
+    fn height(&self, config: &Config, cx: &Context) -> f32;
 
     fn is_expandable(&self) -> bool;
 
@@ -158,34 +172,34 @@ impl Summary {
         last: UtilPoint,
         level: i32,
         max_level: i32,
-        settings: &mut Settings,
+        cx: &mut Context,
     ) {
         let time = (first.time + last.time) * 0.5;
         let util = (first.util + last.util) * 0.5;
-        let diff = (settings.rng.gen::<f32>() - 0.5) / 1.2_f32.powi(max_level - level);
+        let diff = (cx.rng.gen::<f32>() - 0.5) / 1.2_f32.powi(max_level - level);
         let util = (util + diff).at_least(0.0).at_most(1.0);
         let point = UtilPoint { time, util };
         if level > 0 {
-            self.generate_point(first, point, level - 1, max_level, settings);
+            self.generate_point(first, point, level - 1, max_level, cx);
         }
         self.utilization.push(point);
         if level > 0 {
-            self.generate_point(point, last, level - 1, max_level, settings);
+            self.generate_point(point, last, level - 1, max_level, cx);
         }
     }
 
-    fn generate(&mut self, settings: &mut Settings) {
+    fn generate(&mut self, cx: &mut Context) {
         const LEVELS: i32 = 8;
         let first = UtilPoint {
             time: 0.0,
-            util: settings.rng.gen(),
+            util: cx.rng.gen(),
         };
         let last = UtilPoint {
             time: 1.0,
-            util: settings.rng.gen(),
+            util: cx.rng.gen(),
         };
         self.utilization.push(first);
-        self.generate_point(first, last, LEVELS, LEVELS, settings);
+        self.generate_point(first, last, LEVELS, LEVELS, cx);
         self.utilization.push(last);
     }
 }
@@ -198,13 +212,20 @@ impl Entry for Summary {
         "Utilization Plot of Average Usage Over Time"
     }
 
-    fn content(&mut self, ui: &mut egui::Ui, rect: Rect, _viewport: Rect, settings: &mut Settings) {
+    fn content(
+        &mut self,
+        ui: &mut egui::Ui,
+        rect: Rect,
+        _viewport: Rect,
+        _config: &mut Config,
+        cx: &mut Context,
+    ) {
         const TOOLTIP_RADIUS: f32 = 4.0;
         let response = ui.allocate_rect(rect, egui::Sense::hover());
         let hover_pos = response.hover_pos(); // where is the mouse hovering?
 
         if self.utilization.is_empty() {
-            self.generate(settings);
+            self.generate(cx);
         }
 
         let style = ui.style();
@@ -253,9 +274,9 @@ impl Entry for Summary {
         }
     }
 
-    fn height(&self, settings: &Settings) -> f32 {
+    fn height(&self, _config: &Config, cx: &Context) -> f32 {
         const ROWS: u64 = 4;
-        ROWS as f32 * settings.row_height
+        ROWS as f32 * cx.row_height
     }
 
     fn is_expandable(&self) -> bool {
@@ -305,7 +326,14 @@ impl Entry for Slot {
         &self.long_name
     }
 
-    fn content(&mut self, ui: &mut egui::Ui, rect: Rect, viewport: Rect, _settings: &mut Settings) {
+    fn content(
+        &mut self,
+        ui: &mut egui::Ui,
+        rect: Rect,
+        viewport: Rect,
+        _config: &mut Config,
+        _cx: &mut Context,
+    ) {
         let response = ui.allocate_rect(rect, egui::Sense::hover());
         let mut hover_pos = response.hover_pos(); // where is the mouse hovering?
 
@@ -379,8 +407,8 @@ impl Entry for Slot {
         }
     }
 
-    fn height(&self, settings: &Settings) -> f32 {
-        self.rows() as f32 * settings.row_height
+    fn height(&self, _config: &Config, cx: &Context) -> f32 {
+        self.rows() as f32 * cx.row_height
     }
 
     fn is_expandable(&self) -> bool {
@@ -399,7 +427,8 @@ impl<S: Entry> Panel<S> {
         viewport: Rect,
         slot: &mut T,
         y: &mut f32,
-        settings: &mut Settings,
+        config: &mut Config,
+        cx: &mut Context,
     ) -> bool {
         const LABEL_WIDTH: f32 = 60.0;
         const COL_PADDING: f32 = 4.0;
@@ -408,7 +437,7 @@ impl<S: Entry> Panel<S> {
         // Compute the size of this slot
         // This is in screen (i.e., rect) space
         let min_y = *y;
-        let max_y = min_y + slot.height(settings);
+        let max_y = min_y + slot.height(config, cx);
         *y = max_y + ROW_PADDING;
 
         // Cull if out of bounds
@@ -436,14 +465,14 @@ impl<S: Entry> Panel<S> {
         // Note: viewport.min is NOT necessarily (0, 0)
         let content_viewport = viewport.translate(Vec2::new(0.0, rect.min.y - min_y));
 
-        slot.content(ui, content_subrect, content_viewport, settings);
+        slot.content(ui, content_subrect, content_viewport, config, cx);
         slot.label(ui, label_subrect);
 
         false
     }
 
-    fn is_slot_visible(parent_level: u64, index: u64, settings: &Settings) -> bool {
-        parent_level != 0 || (index >= settings.min_node && index <= settings.max_node)
+    fn is_slot_visible(parent_level: u64, index: u64, config: &Config) -> bool {
+        parent_level != 0 || (index >= config.min_node && index <= config.max_node)
     }
 }
 
@@ -455,49 +484,56 @@ impl<S: Entry> Entry for Panel<S> {
         &self.long_name
     }
 
-    fn content(&mut self, ui: &mut egui::Ui, rect: Rect, viewport: Rect, settings: &mut Settings) {
+    fn content(
+        &mut self,
+        ui: &mut egui::Ui,
+        rect: Rect,
+        viewport: Rect,
+        config: &mut Config,
+        cx: &mut Context,
+    ) {
         let mut y = rect.min.y;
         if let Some(summary) = &mut self.summary {
-            Self::render(ui, rect, viewport, summary, &mut y, settings);
+            Self::render(ui, rect, viewport, summary, &mut y, config, cx);
         }
 
         if self.expanded {
             for (i, slot) in self.slots.iter_mut().enumerate() {
                 // Apply visibility settings
-                if !Self::is_slot_visible(self.level, i as u64, settings) {
+                if !Self::is_slot_visible(self.level, i as u64, config) {
                     continue;
                 }
 
-                if Self::render(ui, rect, viewport, slot, &mut y, settings) {
+                if Self::render(ui, rect, viewport, slot, &mut y, config, cx) {
                     break;
                 }
             }
         }
     }
 
-    fn height(&self, settings: &Settings) -> f32 {
+    fn height(&self, config: &Config, cx: &Context) -> f32 {
         const UNEXPANDED_ROWS: u64 = 2;
         const ROW_PADDING: f32 = 4.0;
 
         let mut total = 0.0;
         let mut rows: i64 = 0;
         if let Some(summary) = &self.summary {
-            total += summary.height(settings);
+            total += summary.height(config, cx);
             rows += 1;
         } else if !self.expanded {
             // Need some minimum space if this panel has no summary and is collapsed
-            total += UNEXPANDED_ROWS as f32 * settings.row_height;
+            total += UNEXPANDED_ROWS as f32 * cx.row_height;
             rows += 1;
         }
 
         if self.expanded {
             for (i, slot) in self.slots.iter().enumerate() {
                 // Apply visibility settings
-                if !Self::is_slot_visible(self.level, i as u64, settings) {
+                if !Self::is_slot_visible(self.level, i as u64, config) {
                     continue;
                 }
 
-                total += slot.height(settings);
+                total += slot.height(config, cx);
                 rows += 1;
             }
         }
@@ -517,26 +553,24 @@ impl<S: Entry> Entry for Panel<S> {
 }
 
 impl Window {
-    fn content(&mut self, ui: &mut egui::Ui) {
-        // Use body font to figure out how tall to draw rectangles.
-        let font_id = TextStyle::Body.resolve(ui.style());
-        let row_height = ui.fonts().row_height(&font_id);
-        // Just set this on every frame for now
-        self.settings.row_height = row_height;
+    fn content(&mut self, ui: &mut egui::Ui, cx: &mut Context) {
+        if self.panel.slots.is_empty() {
+            self.generate(cx);
+        }
 
-        ui.heading("Profile 0");
+        ui.heading(format!("Profile {}", self.index));
 
         ScrollArea::vertical()
             .auto_shrink([false; 2])
             .show_viewport(ui, |ui, viewport| {
-                let height = self.panel.height(&self.settings);
+                let height = self.panel.height(&self.config, cx);
                 ui.set_height(height);
                 ui.set_width(ui.available_width());
 
                 let rect = Rect::from_min_size(ui.min_rect().min, viewport.size());
 
                 // Root panel has no label
-                self.panel.content(ui, rect, viewport, &mut self.settings);
+                self.panel.content(ui, rect, viewport, &mut self.config, cx);
 
                 // Draw vertical line through cursor
                 let response = ui.allocate_rect(ui.min_rect(), egui::Sense::hover());
@@ -560,8 +594,8 @@ impl Window {
     fn node_selection(&mut self, ui: &mut egui::Ui) {
         ui.heading("Node Selection");
         let total = self.panel.slots.len().saturating_sub(1) as u64;
-        let min_node = &mut self.settings.min_node;
-        let max_node = &mut self.settings.max_node;
+        let min_node = &mut self.config.min_node;
+        let max_node = &mut self.config.max_node;
         ui.add(Slider::new(min_node, 0..=total).text("First"));
         if *min_node > *max_node {
             *max_node = *min_node;
@@ -604,29 +638,15 @@ impl Window {
 
     fn controls(&mut self, ui: &mut egui::Ui) {
         const WIDGET_PADDING: f32 = 8.0;
-        ui.heading("Profile 0: Controls");
+        ui.heading(format!("Profile {}: Controls", self.index));
         ui.add_space(WIDGET_PADDING);
         self.node_selection(ui);
         ui.add_space(WIDGET_PADDING);
         self.expand_collapse(ui);
     }
-}
 
-impl ProfApp {
-    /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customized the look at feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        let mut result: Self = if let Some(storage) = cc.storage {
-            eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
-        } else {
-            Default::default()
-        };
-
-        result.window.kinds = vec![
+    fn generate(&mut self, cx: &mut Context) {
+        self.kinds = vec![
             "CPU".to_string(),
             "GPU".to_string(),
             "OMP".to_string(),
@@ -636,18 +656,17 @@ impl ProfApp {
             "SysMem".to_string(),
         ];
 
-        let rng = &mut result.window.settings.rng;
         const NODES: i32 = 8192;
         const PROCS: i32 = 8;
         let mut node_slots = Vec::new();
         for node in 0..NODES {
             let mut kind_slots = Vec::new();
             let colors = &[Color32::BLUE, Color32::GREEN, Color32::RED, Color32::YELLOW];
-            for (i, kind) in result.window.kinds.iter().enumerate() {
+            for (i, kind) in self.kinds.iter().enumerate() {
                 let color = colors[i % colors.len()];
                 let mut proc_slots = Vec::new();
                 for proc in 0..PROCS {
-                    let rows: u64 = rng.gen_range(0..64);
+                    let rows: u64 = cx.rng.gen_range(0..64);
                     let items = Vec::new();
                     // Leave items empty, we'll generate it later
                     proc_slots.push(Slot {
@@ -683,7 +702,7 @@ impl ProfApp {
                 slots: kind_slots,
             });
         }
-        result.window.panel = Panel {
+        self.panel = Panel {
             expanded: true,
             short_name: "root".to_owned(),
             long_name: "root".to_owned(),
@@ -691,8 +710,27 @@ impl ProfApp {
             summary: None,
             slots: node_slots,
         };
-        result.window.settings.min_node = 0;
-        result.window.settings.max_node = result.window.panel.slots.len() as u64 - 1;
+        self.config.min_node = 0;
+        self.config.max_node = self.panel.slots.len() as u64 - 1;
+    }
+}
+
+impl ProfApp {
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customized the look at feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        // Load previous app state (if any).
+        // Note that you must enable the `persistence` feature for this to work.
+        let mut result: Self = if let Some(storage) = cc.storage {
+            eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
+        } else {
+            Default::default()
+        };
+
+        result.windows.clear();
+        result.windows.push(Window::default());
 
         #[cfg(not(target_arch = "wasm32"))]
         {
@@ -712,9 +750,11 @@ impl eframe::App for ProfApp {
     /// Called each time the UI needs repainting.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let Self {
-            window,
+            windows,
+            cx,
             #[cfg(not(target_arch = "wasm32"))]
             last_update,
+            ..
         } = self;
 
         let mut _fps = 0.0;
@@ -744,14 +784,24 @@ impl eframe::App for ProfApp {
             const WIDGET_PADDING: f32 = 8.0;
             ui.add_space(WIDGET_PADDING);
 
-            egui::Frame::group(ui.style()).show(ui, |ui| {
-                ui.set_width(ui.available_width()); // fill side panel
-                window.controls(ui);
-            });
+            for window in windows.iter_mut() {
+                egui::Frame::group(ui.style()).show(ui, |ui| {
+                    ui.set_width(ui.available_width());
+                    window.controls(ui);
+                });
+            }
 
+            if ui.button("Add Another Profile").clicked() {
+                let mut index = 0;
+                if let Some(last) = windows.last() {
+                    index = last.index + 1;
+                }
+                windows.push(Window::default());
+                windows.last_mut().unwrap().index = index;
+            }
 
             egui::Frame::group(ui.style()).show(ui, |ui| {
-                ui.set_width(ui.available_width()); // fill side panel
+                ui.set_width(ui.available_width());
                 ui.heading("Task Details");
                 ui.label("Click on a task to see it displayed here.");
             });
@@ -769,6 +819,8 @@ impl eframe::App for ProfApp {
                     ui.label(".");
                 });
 
+                egui::warn_if_debug_build(ui);
+
                 #[cfg(not(target_arch = "wasm32"))]
                 {
                     ui.separator();
@@ -778,8 +830,23 @@ impl eframe::App for ProfApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            window.content(ui);
-            egui::warn_if_debug_build(ui);
+            // Use body font to figure out how tall to draw rectangles.
+            let font_id = TextStyle::Body.resolve(ui.style());
+            let row_height = ui.fonts().row_height(&font_id);
+            // Just set this on every frame for now
+            cx.row_height = row_height;
+
+            let mut remaining = windows.len();
+            for window in windows.iter_mut() {
+                egui::Frame::group(ui.style()).show(ui, |ui| {
+                    ui.push_id(window.index, |ui| {
+                        ui.set_height(ui.available_height() / (remaining as f32));
+                        ui.set_width(ui.available_width());
+                        window.content(ui, cx);
+                        remaining -= 1;
+                    });
+                });
+            }
         });
     }
 }
